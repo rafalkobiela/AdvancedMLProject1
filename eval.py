a491c58a89fa7f8bb107debc11fc53b6f6a37edf
rafalkobiela <kobiela.rafal@gmail.com>
2018-10-28 19:37:47 +0100

    test gita pycharm

--- eval.py
+++ eval.py
@@ -3,9 +3,7 @@ import numpy as np
 
 def precision_at_10(y_test, pred):
 
-    if len(y_test) != len(pred) or \
-       len(y_test.shape) != 1 or \
-       len(pred.shape) != 1:
+    if len(y_test) != len(pred):
         raise Exception("Lengths of y_test and probabilities vector are different!")
 
     sorted_index = np.argsort(pred)[::-1]
