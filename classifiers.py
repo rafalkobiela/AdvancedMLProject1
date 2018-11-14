45da521fafafc155dadf5ddbbe1b0d8c0359a3fe
Rafal Kobiela <rkobiela001@cee.ema.ad.pwcinternal.com>
2018-11-14 13:45:46 +0100

    adding LightGBM

--- classifiers.py
+++ classifiers.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-import xgboost as xgb
+# import xgboost as xgb
 from sklearn import metrics
+import lightgbm as lgb
 import os
 import eval
 
@@ -65,26 +66,50 @@ print("AdaBoost:",
 
 # XGBoost
 
-dtrain = xgb.DMatrix(X_train, label=y_train)
-dtest = xgb.DMatrix(X_test, label=y_test)
+# dtrain = xgb.DMatrix(X_train, label=y_train)
+# dtest = xgb.DMatrix(X_test, label=y_test)
 
-param = {'max_depth': 3,
-         'eta': 0.9,
-         'silent': 1,
-         'objective': 'binary:logistic',
-         'nthread': 4,
-         'eval_metric': 'auc'}
+# param = {'max_depth': 3,
+#          'eta': 0.9,
+#          'silent': 1,
+#          'objective': 'binary:logistic',
+#          'nthread': 4,
+#          'eval_metric': 'auc'}
 
-evallist = [(dtest, 'eval'), (dtrain, 'train')]
+# evallist = [(dtest, 'eval'), (dtrain, 'train')]
 
-num_round = 10
+# num_round = 10
 
-bst = xgb.train(param, dtrain, 10, evallist, verbose_eval=0)
+# bst = xgb.train(param, dtrain, 10, evallist, verbose_eval=0)
 
-prob = bst.predict(dtest)
+# prob = bst.predict(dtest)
 
-print("XGBoost:",
-      eval.precision_at_10(y_test, prob))
+# print("XGBoost:",
+#       eval.precision_at_10(y_test, prob))
 
 
+# LGBM
 
+params = {
+    'objective' :'binary',
+    'learning_rate' : 0.02,
+    'num_leaves' : 76,
+    'feature_fraction': 0.64, 
+    'bagging_fraction': 0.8, 
+    'bagging_freq':1,
+    'boosting_type' : 'gbdt',
+    'metric': 'binary_logloss',
+    'is_unbalance' : True,
+    'n_jobs' : 3
+}
+
+clf = lgb.LGBMClassifier(
+   **params
+)
+clf.fit(X_train, y_train)
+
+prob = clf.predict_proba(X_test)
+clases = clf.predict(X_test)
+
+print("LGBM:",
+      eval.precision_at_10(y_test, prob[:, 1]))
